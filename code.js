b0aa5ec6869dd87e561a5cd8606a17c1e8fd30e4
MKdivine <jensboerger23@gmail.com>
2024-12-02 06:21:51 +0100

    start

--- code.js
+++ code.js
@@ -4,8 +4,8 @@ containerDiv.classList.add("container");
 document.body.appendChild(containerDiv);
 
 // Quadrate hinzufügen
-for (let i = 0; i < 256; i++) { // Es sollten genau 256 Quadrate sein, nicht 257
+for (let i = 0; i < 256; i++) { // Es sollten genau 256 Quadrate sein
   const square = document.createElement("div");
   square.classList.add("square");
-  containerDiv.appendChild(square); // Direkt ins Container-Div einfügen
+  containerDiv.appendChild(square); // ins Container-Div einfügen
 }
