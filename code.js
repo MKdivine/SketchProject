c9323e4f66298dc156c8cff8b6341bf8bc767ca2
MKdivine <jensboerger23@gmail.com>
2024-12-02 07:29:00 +0100

    start

--- code.js
+++ code.js
@@ -3,42 +3,36 @@ const containerDiv = document.createElement("div");
 containerDiv.classList.add("container");
 document.body.appendChild(containerDiv);
 
-
-
-
-
 // random color
 function getRandomColor() {
-    const r = Math.floor(Math.random() * 256); // Rot: 0-255
-    const g = Math.floor(Math.random() * 256); // Grün: 0-255
-    const b = Math.floor(Math.random() * 256); // Blau: 0-255
-    return `rgb(${r}, ${g}, ${b})`;
-  }
-  
+  const r = Math.floor(Math.random() * 256); // Rot: 0-255
+  const g = Math.floor(Math.random() * 256); // Grün: 0-255
+  const b = Math.floor(Math.random() * 256); // Blau: 0-255
+  return `rgb(${r}, ${g}, ${b})`;
+}
 
 // Quadrate hinzufügen
-for (let i = 0; i < 256; i++) { // Es sollten genau 256 Quadrate sein
+for (let i = 0; i < 256; i++) {
+  // Es sollten genau 256 Quadrate sein
   const square = document.createElement("div");
   square.classList.add("square");
-  for (let i = 0; i < 256; i++) {
-      
-  }
+  for (let i = 0; i < 256; i++) {}
   containerDiv.appendChild(square); // ins Container-Div einfügen
 }
 
 // Event Listener für die Maus
-containerDiv.addEventListener("mouseover", function(event) {
+containerDiv.addEventListener("mouseover", function (event) {
   if (event.target.classList.contains("square")) {
     event.target.style.backgroundColor = "black";
   }
-})
+});
 
-containerDiv.addEventListener("contextmenu", function(event) {
+containerDiv.addEventListener("contextmenu", function (event) {
   if (event.target.classList.contains("square")) {
     event.target.style.backgroundColor = getRandomColor();
     const h3 = document.createElement("h3");
     h3.innerText = "Kathi";
-    h3.style.color = getRandomColor()
+    h3.style.color = getRandomColor();
     event.target.appendChild(h3);
   }
-})
+});
