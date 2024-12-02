38a7fe15311b01a1f78e169eb41cdc9b26b630bf
MKdivine <jensboerger23@gmail.com>
2024-12-02 07:01:46 +0100

    start

--- code.js
+++ code.js
@@ -3,9 +3,35 @@ const containerDiv = document.createElement("div");
 containerDiv.classList.add("container");
 document.body.appendChild(containerDiv);
 
+
+// random color
+function getRandomColor() {
+    const r = Math.floor(Math.random() * 256); // Rot: 0-255
+    const g = Math.floor(Math.random() * 256); // Grün: 0-255
+    const b = Math.floor(Math.random() * 256); // Blau: 0-255
+    return `rgb(${r}, ${g}, ${b})`;
+  }
+  
+
 // Quadrate hinzufügen
 for (let i = 0; i < 256; i++) { // Es sollten genau 256 Quadrate sein
   const square = document.createElement("div");
   square.classList.add("square");
+  for (let i = 0; i < 256; i++) {
+      
+  }
   containerDiv.appendChild(square); // ins Container-Div einfügen
 }
+
+// Event Listener für die Maus
+containerDiv.addEventListener("mouseover", function(event) {
+  if (event.target.classList.contains("square")) {
+    event.target.style.backgroundColor = "white";
+  }
+})
+
+containerDiv.addEventListener("mouseout", function(event) {
+  if (event.target.classList.contains("square")) {
+    event.target.style.backgroundColor = getRandomColor();
+  }
+})
